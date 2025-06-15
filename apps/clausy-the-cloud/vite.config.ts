009ddae1e5e54ebdb529a0376ac975e120bd4ce0
Adam Alix <adam.alix@gmail.com>
2025-06-14 20:13:18 -0400

    fix asset paths

--- apps/clausy-the-cloud/vite.config.ts
+++ apps/clausy-the-cloud/vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/clausy-the-cloud/', // Set base for GitHub Pages subfolder deployment
   plugins: [react()],
 });
