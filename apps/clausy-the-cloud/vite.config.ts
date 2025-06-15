4f97d04794a86de8d0fb0ab2a3c4c8a8443da3f7
Adam Alix <adam.alix@gmail.com>
2025-06-14 20:22:46 -0400

    fix base url

--- apps/clausy-the-cloud/vite.config.ts
+++ apps/clausy-the-cloud/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/clausy-the-cloud/', // Set base for GitHub Pages subfolder deployment
+  base: '/vanessa-games/clausy-the-cloud/', // Correct base for GitHub Pages deployment
   plugins: [react()],
 });
