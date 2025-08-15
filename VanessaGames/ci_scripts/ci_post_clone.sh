71389f841a07d01df62dd11f487163344e81dce5
Adam Alix <adam.alix@gmail.com>
2025-08-15 17:27:48 -0400

    try to fix post clone

--- VanessaGames/ci_scripts/ci_post_clone.sh
+++ VanessaGames/ci_scripts/ci_post_clone.sh
@@ -6,9 +6,8 @@ curl https://mise.run | sh
 export PATH="$HOME/.local/bin:$PATH"
 
 # Install tools from .mise.toml in repo root
-cd "$CI_PRIMARY_REPOSITORY_PATH"
 mise install
-eval "$(mise activate sh --shims)"
+eval "$(mise activate bash --shims)"
 
 # Navigate to VanessaGames directory and generate Xcode project
 cd "$CI_PRIMARY_REPOSITORY_PATH/VanessaGames"
